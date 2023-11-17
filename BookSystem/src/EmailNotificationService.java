efb2aead2c823f649673c59f4d9a172046418a0b
Aaron Wang <aaronwang@Aarons-MacBook-Pro.local>
2023-11-17 15:15:06 -0400

    Delete unneeded folders

--- BookSystem/src/EmailNotificationService.java
+++
@@ -1,7 +0,0 @@
-class EmailNotificationService implements NotificationService {
-    @Override
-    public void sendNotification(String message) {
-        // Code to send an email notification
-        System.out.println("Email notification: " + message);
-    }
-}
--- BookSystem/src/SMSPushNotificationService.java
+++
@@ -1,7 +0,0 @@
-class SMSPushNotificationService implements NotificationService {
-    @Override
-    public void sendNotification(String message) {
-        // Code to send an SMS notification
-        System.out.println("SMS notification: " + message);
-    }
-}
